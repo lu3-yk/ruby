ff52985598ee177aa8402fedaf8d5b20251e81e4
wadayuki <106656539+lu3-yk@users.noreply.github.com>
2022-08-25 12:14:30 +0000

    FizzBuzz復習

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,14 +1,17 @@
 def fizz_buzz(number)
+  # ここにFizzBuzzプログラムの処理を書いてください
   if number %15 == 0
     "FizzBuzz"
-elsif number %3 ==0
-      "Fizz"
-elsif number %5 == 0
-     "Buzz"
-
-else 
+  elsif number %3 == 0
+    "Fizz"
+  elsif number %5 == 0
+    "Buzz"
+  else
     number.to_s
-end    
+  end
+
+
+
 end
 
 puts "数字を入力してください。"
@@ -17,3 +20,26 @@ input = gets.to_i
 
 puts "結果は..."
 puts fizz_buzz(input)
+
+
+
+
+# def fizz_buzz(number)
+#   if number %15 == 0
+#     "FizzBuzz"
+# elsif number %3 ==0
+#       "Fizz"
+# elsif number %5 == 0
+#     "Buzz"
+
+# else
+#     number.to_s
+# end
+# end
+
+# puts "数字を入力してください。"
+
+# input = gets.to_i
+
+# puts "結果は..."
+# puts fizz_buzz(input)
